634d09b527e8b772fc1223961dc51b90cc3bd459
fnursaray <fatmanursaray@icloud.com>
2024-07-28 17:11:07 +0300

    finish

---
+++ README.md
@@ -0,0 +1,18 @@
+# Job-App
+
+## Libraries
+
+- React
+- React Icons
+- React Router DOM
+- React Redux
+- Redux Toolkit
+- Axios
+- JSON Server
+- React Toastify
+- SASS
+- UUID
+
+## Screen Gif
+
+<img src="screen.gif" />
---
+++ src/components/Loader.jsx
@@ -0,0 +1,16 @@
+const Loader = () => {
+  return (
+    <div className="wrapperr">
+      <div className="wrapper">
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="shadow"></div>
+        <div className="shadow"></div>
+        <div className="shadow"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
